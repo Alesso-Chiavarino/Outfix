d812b359ab748545861ab63b2aa66c5f7cebe415
alesso-chiavarino <alessochiavarino@gmail.com>
2024-03-31 02:44:39 -0300

    Deploy in Vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,11 @@ import { CardsModuleOne } from "@/components/CardsModuleOne";
 import { CardsModuleThree } from "@/components/CardsModuleThree";
 import { CardsModuleTwo } from "@/components/CardsModuleTwo";
 import { Newsletter } from "@/components/Newsletter";
-import NextNProgress from 'nextjs-progressbar';
 
 export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center gap-20">
-      <NextNProgress
-        color="#29D"
-        startPosition={0.3}
-        stopDelayMs={200}
-        height={3}
-      />
       <Banner />
       <CardsModuleOne />
       <CardsModuleTwo />
