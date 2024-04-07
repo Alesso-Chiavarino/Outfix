07c51f1336913508d735c4f9adf2d3b212d3518e
alesso-chiavarino <alessochiavarino@gmail.com>
2024-04-07 16:57:33 -0300

    remove page admin

--- src/app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-
-// export default function AdminPage() {
-//   return (
-//     <main className='flex min-h-screen flex-col items-center gap-20'>
-//       <div>DashboardPage</div>
-//     </main>
-//   )
-// }
